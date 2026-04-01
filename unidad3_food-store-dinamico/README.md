62841aec31d66f432e6a7c303e24e41f3c7c94a7
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2026-04-01 17:46:42 -0300

    feat(unidad3-food-store-dinamico): crear base del food store dinamico

---
+++ unidad3_food-store-dinamico/README.md
@@ -0,0 +1,11 @@
+# Trabajo Práctico - Unidad 3 (JavaScript) - Volver dinámico al Food Store
+
+✨ Estudiante
+
+- Nombre: Varela, Santiago Octavio
+- Comisión: M25 C3-13
+- Email institucional: santiago.varela@tupad.utn.edu.ar
+
+Repositorio donde podrán encontrar mis trabajos de Programación III: https://github.com/santiagovOK/UTN-TUPaD-P3
+
+---
---
+++ unidad3_food-store-dinamico/js/data.js
@@ -0,0 +1,19 @@
+const categorias = ["Hamburguesas", "Pizzas", "Papas Fritas", "Bebidas"]; 
+const productos = [ 
+   { 
+       id: 1, 
+       nombre: "Hamburguesa Triple", 
+       descripcion: "Triple carne, cheddar y bacon", 
+       precio: 25000, 
+       imagen: "assets/img/burger.jpg", 
+       categoria: "Hamburguesas" 
+   }, 
+   { 
+       id: 2, 
+       nombre: "Pizza Muzzarella", 
+       descripcion: "Salsa casera y orégano", 
+       precio: 18000, 
+       imagen: "assets/img/pizza.jpg", 
+       categoria: "Pizzas" 
+   },  
+];
