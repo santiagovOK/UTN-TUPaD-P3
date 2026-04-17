4dd6cfe74a42855c52b35a0b9ad91d4f6cfa2bbc
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2026-04-17 15:56:24 -0300

    feat(unidad5): agregar base de entidades y diagrama uml

--- TP_ToString_Colecciones/src/entities/Categoria.java
+++ TP_ToString_Colecciones/src/entities/Categoria.java
@@ -6,6 +6,59 @@ package entities;
  * <santiago.varela@tupad.utn.edu.ar>
  */
 
-public class Categoria {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Categoria extends Base {
+    private String nombre;
+    private String descripcion;
+    private Set<Producto> productos; // Relación de uno a muchos con Producto
+
+    public Categoria() {
+        super();
+        this.productos = new HashSet<>();
+    }
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+    }
+
+    // Getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Set<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setProductos(Set<Producto> productos) {
+        this.productos = productos;
+    }
+
+    //
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", productos=" + productos +
+                '}';
+    }
+
+
 }
 
--- TP_ToString_Colecciones/src/entities/Producto.java
+++ TP_ToString_Colecciones/src/entities/Producto.java
@@ -8,5 +8,13 @@ package entities;
  */
 
 public class Producto {
+    private String nombre;
+    private Double precio;
+    private String descripcion;
+    private int stock;
+    private String imagen;
+    private boolean disponible;
+
+
 }
 
